791476ed8b37176f0a80c27807b4281c52877bcf
Vasilisa <vasilisavygovskaya1109@gmail.com>
2023-12-02 13:08:06 +0300

    02-12-2023-dz

---
+++ project/script_js/script.js
@@ -0,0 +1,15 @@
+let m = document.querySelector(".menu");
+let nav = document.querySelector(".main-menu");
+
+if(m){
+    m.addEventListener("click", (e) => {
+        if(m.classList.contains("pushed")){
+            m.classList.remove("pushed");
+            nav.classList.remove("opened");
+        }
+        else{
+            m.classList.add("pushed");
+            nav.classList.add("opened");
+        }
+    });
+}
