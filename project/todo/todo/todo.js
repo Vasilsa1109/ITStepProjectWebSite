bdd545421b895c3f9b748c2dce746089e893e914
Vasilisa <vasilisavygovskaya1109@gmail.com>
2023-12-07 21:00:17 +0300

    dz-07-12-remove_and_check

--- project/todo/todo/todo.js
+++ project/todo/todo/todo.js
@@ -6,9 +6,18 @@ function addTask(event){
     list.insertAdjacentHTML(
         "beforeend", 
         `<div class="task">
-                    <div>${tasks.length+1}, ${input.value}</div> 
-                    <button class="but"></button>
-                    <button class="but1"></button>
+                    <div>${tasks.length+1} .  ${input.value} </div>
+                        <button class="but"></button>
+                        <button class="but1"></button>
                 </div>`
-                );
-}
+    );
+    }
+
+    function checkTask(event) {
+      let button = event.target.parentElement;
+      button.style.textDecoration = "line-through";
+    }
+    function removeTask(event) {
+      confirm("Вы действительно хотите удалить задачу?")
+      event.target.parentNode.remove();
+    }
