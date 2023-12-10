641708cf5e0a8777635d3e402accf57a2b6bce83
= <=>
2023-12-10 19:46:28 +0300

    10-12-registr

--- project/todo/todo/todo.js
+++ project/todo/todo/todo.js
@@ -1,23 +1,31 @@
-function addTask(event){
+function addTask(){
     let list = document.querySelector(".tasks");
     let tasks  = document.getElementsByClassName("task");
     let input = document.querySelector(`input[name="todo-text"]`);
 
-    list.insertAdjacentHTML(
-        "beforeend", 
-        `<div class="task">
-                    <div>${tasks.length+1} .  ${input.value} </div>
-                        <button class="but"></button>
-                        <button class="but1"></button>
-                </div>`
-    );
+    if(input.value.trim() != ""){
+            list.insertAdjacentHTML(
+            "beforeend", 
+            `<div class="task">
+                        <div>${tasks.length+1}.  ${input.value} </div>
+                            <button class="but"></button>
+                            <button class="but1"></button>
+                    </div>`   
+        );
+    }
+    else{
+        alert("Enter a value");
+    }
+}
+    function checkTask(event) {
+    //   let button = event.target.parentElement;
+    //   button.style.textDecoration = "line-through";
+    event.target.previousElementSibling.style.textDecoration = "line-through";
     }
 
-    function checkTask(event) {
-      let button = event.target.parentElement;
-      button.style.textDecoration = "line-through";
-    }
     function removeTask(event) {
-      confirm("Вы действительно хотите удалить задачу?")
-      event.target.parentNode.remove();
-    }
+      if(confirm("Вы действительно хотите удалить задачу?")){
+        event.target.parentElement.remove();
+      }}
+
+    //   event.target.parentNode.remove();
